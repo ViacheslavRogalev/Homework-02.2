52c074356e8eb5fdde6748c5dfbea5c7a0b35850
vyahislav <khv-rvv@mail.ru>
2023-04-21 00:23:22 +1000

    change main

--- main.cpp
+++ main.cpp
@@ -6,32 +6,37 @@ struct Bank_Account
 {
 	int account_num;
 	std::string first_name;
-	float amount_of_money;
+	double amount_of_money;
 };
+
+void change_balance (Bank_Account* pers, double new_amount_of_money)
+{
+	pers->amount_of_money = new_amount_of_money;
+}
 
 int main(int argc, char** argv)
 {
 	SetConsoleCP(1251);
 	setlocale(LC_ALL, "Russian");
 
-	int a;
-
 	Bank_Account pers;
-	Bank_Account* p_pers = &pers;
+	double new_amount_of_money = 0;
 
 	std::cout << "¬ведите номер счета: ";
-	std::cin >> p_pers->account_num;
+	std::cin >> pers.account_num;
 
 	std::cout << "¬ведите им€ владельца: ";
-	std::cin >> p_pers->first_name;
+	std::cin >> pers.first_name;
 
 	std::cout << "¬ведите баланс: ";
-	std::cin >> a;
+	std::cin >> pers.amount_of_money;
 
 	std::cout << "¬ведите новый баланс: ";
-	std::cin >> p_pers->amount_of_money;
+	std::cin >> new_amount_of_money;
 
-	std::cout << "¬аш счет: " << pers.first_name << ", " << pers.account_num << ", " << pers.amount_of_money;
+	change_balance(&pers, new_amount_of_money);
+
+	std::cout << "¬аш счет: " << pers.first_name << ", " << pers.account_num << ", " << pers.amount_of_money << std::endl;
 
 	return 0;
 }
